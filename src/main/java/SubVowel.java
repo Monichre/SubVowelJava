923111f00074e488b4f6e8d79505938a47b4d9de
Liam Ellis <liamhellis@gmail.com>
2016-04-22 09:09:03 -0700

    add basic file structure, templates, gradle build and test files, spec implementation to come

---
+++ src/main/java/SubVowel.java
@@ -0,0 +1,8 @@
+public class SubVowel {
+  public static void main(String[] args) {}
+
+  // public Boolean isLeapYear(Integer year) {
+  //   // eventually put your code here
+  //   return false;
+  // }
+}
---
+++ src/test/java/SubVowelTest.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LeapYearTest {
+
+  // @Test
+  // public void isLeapYear_forNumberDivisibleByFour_true() {
+  //   // eventually put your testing code here
+  // }
+}
